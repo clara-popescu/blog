d09834f6b440430dd219df1e09e1f579d89c0769
clara <c.popescu0520231@arts.ac.uk>
2025-11-06 21:34:51 +0000

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GradProject from './pages/GradProject'
+import MachineLearning from './pages/MachineLearning';
 
 function App() {
 
   return (
     <>
-      <GradProject/>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<GradProject />} />
+            <Route path="/pages/MachineLearning" element={<MachineLearning />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
--- src/pages/MachineLearning.jsx
+++ src/pages/MachineLearning.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from '../components/Header'
+import Blogs from '../components/Blogs'
+import Footer from '../components/Footer'
+import posts from '../data/machine-learning/posts.json'
 
-const MachineLearning = () => {
+function MachineLearning() {
   return (
-    <div>MachineLearning</div>
+    <>
+      <Header/>
+      <p className='text-gray-500 text-center text-sm w-screen'>All posts were written at the end of the week, based on notes I took throughout the week as I worked on various aspects of the project.</p>
+        {posts.map(post => (
+          <Blogs key={post.id} post={post} />
+        ))}
+        <Footer/>
+    </>
   )
 }
 
